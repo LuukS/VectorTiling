84f8847d556f83d3b35df173b6a050547e7a047f
LuukS <luuk.schaminee@gmail.com>
2018-07-10 12:05:02 +0200

    Eerste versie

---
+++ main.js
@@ -0,0 +1,15 @@
+//mapboxgl.accessToken = 'pk.eyJ1IjoibmllbmViIiwiYSI6IlR6NWEzdmMifQ.dRVGdAluvTb9EIXVBREbzw';
+//var map = new mapboxgl.Map({
+    //container: 'map',
+    //style: 'mapbox://styles/nieneb/cjg3h8yp80oi82rldxukpu0oi',
+//});
+
+var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'style.json',
+        hash: true,
+        zoom: 11,
+        pitch: 90,
+        bearing: 90,
+        center: [ 5.24163, 52.36438]
+    });
